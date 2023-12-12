b14eefde55b0cdf93b13868878125923a15f9e25
DNS <37093+DNS@users.noreply.github.com>
2023-12-12 19:44:08 +0700

    M: install-freebsd.sh

--- install-freebsd.sh
+++ install-freebsd.sh
@@ -2,4 +2,5 @@
 
 cp hieamtsrn-freebsd.kbd /usr/share/syscons/keymaps/hieamtsrn-freebsd.kbd
 
-echo keymap=\"/usr/share/syscons/keymaps/hieamtsrn-freebsd.kbd\" >> /etc/rc.conf
+#echo keymap=\"/usr/share/syscons/keymaps/hieamtsrn-freebsd.kbd\" >> /etc/rc.conf
+sysrc keymap="/usr/share/syscons/keymaps/hieamtsrn-freebsd.kbd"
